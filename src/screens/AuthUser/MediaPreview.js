73bc9ee65d22d129d4c5ded297514e153591fc59
Min Park <95076634+mpark1@users.noreply.github.com>
2023-12-17 19:13:58 +0900

    Update MediaPreview.js

--- src/screens/AuthUser/MediaPreview.js
+++ src/screens/AuthUser/MediaPreview.js
@@ -9,6 +9,7 @@ import {
   Pressable,
   TextInput,
 } from 'react-native';
+import Video from 'react-native-video';
 import {KeyboardAwareScrollView} from 'react-native-keyboard-aware-scroll-view';
 import AntDesign from 'react-native-vector-icons/AntDesign';
 
@@ -16,25 +17,56 @@ import globalStyle from '../../assets/styles/globalStyle';
 import {scaleFontSize} from '../../assets/styles/scaling';
 
 import BlueButton from '../../components/Buttons/BlueButton';
+import DropDownPicker from 'react-native-dropdown-picker';
+
+import ages from '../../data/ages.json';
 
 const MediaPreview = ({navigation, route}) => {
-  const {mediaType, imageList: initialImageList} = route.params;
-  const [imageList, setImageList] = useState(initialImageList);
+  const {mediaType, mediaList: initialMediaList, age} = route.params;
+  const [mediaList, setMediaList] = useState(initialMediaList);
   console.log('mediaType: ', mediaType);
-  console.log('imageList: ', imageList);
+  console.log('imageList: ', mediaList);
+  const [isDropDownPickerOpen, setIsDropDownPickerOpen] = useState(false);
+  const ageOptions = ages.map(item => ({
+    label: item,
+    value: item,
+  }));
+  const [selectedAge, setSelectedAge] = useState(age);
 
   const handleRemoveItem = index => {
-    const updatedList = [...imageList];
+    const updatedList = [...mediaList];
     updatedList.splice(index, 1);
-    setImageList(updatedList);
+    setMediaList(updatedList);
   };
 
-  const renderFlatList = useCallback(() => {
+  const renderAgeField = () => {
+    return (
+      <View style={styles.ageField}>
+        <Text style={styles.label}>연령</Text>
+        <DropDownPicker
+          containerStyle={styles.dropDownPicker.containerStyle}
+          style={styles.dropDownPicker.borderStyle}
+          dropDownContainerStyle={styles.dropDownPicker.borderStyle}
+          multiple={false}
+          placeholderStyle={styles.dropDownPicker.placeholder}
+          placeholder={'연령을 선택해주세요'}
+          items={ageOptions}
+          value={selectedAge}
+          setValue={setSelectedAge}
+          open={isDropDownPickerOpen}
+          setOpen={setIsDropDownPickerOpen}
+          listMode={'SCROLLVIEW'}
+        />
+      </View>
+    );
+  };
+
+  const renderImageFlatList = useCallback(() => {
     return (
       <FlatList
         scrollEnabled={false}
         numColumns={2}
-        data={imageList}
+        data={mediaList}
         renderItem={({item, index}) => {
           return (
             <View style={styles.imageContainer}>
@@ -53,7 +85,36 @@ const MediaPreview = ({navigation, route}) => {
         }}
       />
     );
-  }, [imageList]);
+  }, [mediaList]);
+
+  const renderEmptyImageList = useCallback(() => {
+    return (
+      <View style={styles.noImagesOrVideo.container}>
+        <Text style={styles.noImagesOrVideo.text}>
+          {
+            '선택된 사진이 없습니다.\n\n이전 화면으로 돌아가 앨범에 등록할 사진을 선택해주세요.'
+          }
+        </Text>
+      </View>
+    );
+  }, []);
+
+  const renderVideo = () => {
+    return (
+      <View style={styles.video.container}>
+        <Video
+          source={{
+            uri: mediaList[0].uri, // 영상은 1개만 가능
+          }}
+          style={styles.video.style}
+          paused={true}
+          resizeMode={'cover'}
+          onLoad={e => console.log(e)}
+          repeat={false}
+        />
+      </View>
+    );
+  };
 
   const renderStoryField = () => {
     return (
@@ -61,10 +122,10 @@ const MediaPreview = ({navigation, route}) => {
         <Text style={[styles.label, {paddingBottom: 16}]}>스토리</Text>
         <TextInput
           style={styles.story.textInput}
-          placeholder={
-            '사진을 보면 떠오르는 추억이 있나요? (최대 300자)\n\n예: [이름]을 처음 봤을 때. 수건에 살포시 쌓여 있던 너에게 첫눈에 반했어.'
-          }
+          placeholder={'사진을 보면 떠오르는 추억이 있나요? (최대 300자)'}
           placeholderTextColor={'#939393'}
+          textAlign={'left'}
+          textAlignVertical={'top'}
           multiline={true}
           autoCorrect={false}
           autoCapitalize={'none'}
@@ -80,7 +141,7 @@ const MediaPreview = ({navigation, route}) => {
       <View style={styles.submitButton}>
         <BlueButton
           title={'등록하기'}
-          disabled={imageList.length === 0}
+          disabled={mediaList.length === 0}
           // onPress={}
         />
       </View>
@@ -91,18 +152,19 @@ const MediaPreview = ({navigation, route}) => {
     <KeyboardAwareScrollView
       style={[globalStyle.flex, globalStyle.backgroundWhite]}>
       <View style={styles.spacer}>
+        {renderAgeField()}
         <Text style={styles.label}>
           {mediaType === 'photo' ? '사진' : '영상'}
         </Text>
-        <View style={styles.flatListContainer}>
-          {imageList.length > 0 ? (
-            renderFlatList()
-          ) : (
-            <Text style={styles.noImagesOrVideo}>
-              이전 화면으로 돌아가 사진이나 영상을 추가해주세요.
-            </Text>
-          )}
-        </View>
+        {mediaType === 'photo' ? (
+          <View style={styles.flatListContainer}>
+            {mediaList.length > 0
+              ? renderImageFlatList()
+              : renderEmptyImageList()}
+          </View>
+        ) : (
+          renderVideo()
+        )}
         {renderStoryField()}
         {renderSubmitButton()}
       </View>
@@ -114,11 +176,30 @@ export default MediaPreview;
 
 const styles = StyleSheet.create({
   spacer: {paddingTop: 10, paddingHorizontal: 20},
+  ageField: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'space-between',
+    marginBottom: Dimensions.get('window').height * 0.03,
+    zIndex: 1,
+  },
   label: {
     fontSize: scaleFontSize(18),
     fontWeight: 'bold',
     paddingBottom: 10,
     color: '#000',
+  },
+  dropDownPicker: {
+    containerStyle: {
+      width: '60%',
+      maxHeight: 30,
+    },
+    borderStyle: {
+      borderRadius: 5,
+      borderColor: '#d9d9d9',
+      minHeight: 30,
+    },
+    placeholder: {color: '#939393', fontSize: scaleFontSize(16)},
   },
   flatListContainer: {
     width: '100%',
@@ -135,6 +216,22 @@ const styles = StyleSheet.create({
     height: '100%',
     borderRadius: 5,
   },
+  video: {
+    container: {
+      width: Dimensions.get('window').width * 0.8,
+      height: Dimensions.get('window').width * 0.8,
+      alignSelf: 'center',
+      marginBottom: Dimensions.get('window').height * 0.03,
+    },
+    style: {
+      position: 'absolute',
+      top: 0,
+      left: 0,
+      bottom: 0,
+      right: 0,
+      borderRadius: 5,
+    },
+  },
   closeCircle: {
     position: 'absolute',
     right: -7,
@@ -143,9 +240,16 @@ const styles = StyleSheet.create({
     borderRadius: 20,
   },
   noImagesOrVideo: {
-    fontSize: scaleFontSize(16),
-    color: '#939393',
-    fontWeight: '500',
+    container: {
+      backgroundColor: '#EEEEEE',
+      padding: 10,
+      borderRadius: 5,
+      marginBottom: Dimensions.get('window').height * 0.03,
+    },
+    text: {
+      fontSize: scaleFontSize(16),
+      color: '#000',
+    },
   },
   story: {
     textInput: {
@@ -162,6 +266,7 @@ const styles = StyleSheet.create({
   },
   submitButton: {
     alignSelf: 'center',
-    marginVertical: Dimensions.get('window').height * 0.03,
+    marginTop: Dimensions.get('window').height * 0.03,
+    marginBottom: Dimensions.get('window').height * 0.05,
   },
 });
