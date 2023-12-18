2507ed6c8d70a1b1240ae6d3560e93aa84a94039
Min Park <95076634+mpark1@users.noreply.github.com>
2023-12-18 10:48:00 +0900

    Update Letters.js

--- src/screens/AuthUser/CenterTabs/Letters.js
+++ src/screens/AuthUser/CenterTabs/Letters.js
@@ -123,6 +123,74 @@ const styles = StyleSheet.create({
     backgroundColor: '#FFF',
     paddingTop: 15,
   },
+  letter: {
+    container: {
+      borderBottomWidth: 1,
+      borderColor: '#D9D9D9',
+      paddingHorizontal: 20,
+      paddingVertical: 13,
+    },
+    title: {
+      color: '#374957',
+      fontSize: scaleFontSize(17),
+      fontWeight: 'bold',
+      paddingBottom: 7,
+    },
+    flexDirectionRow: {
+      flexDirection: 'row',
+    },
+    profilePicContainer: {
+      height: 80,
+      width: 80,
+      marginTop: 8,
+    },
+    profilePic: {
+      width: '100%',
+      height: '100%',
+      borderRadius: 40,
+    },
+    messageContainer: {
+      flex: 1,
+      paddingLeft: 5,
+      marginLeft: 25,
+    },
+    nameRelationshipDateContainer: {
+      flexDirection: 'row',
+      alignItems: 'center',
+      alignSelf: 'flex-end',
+      marginBottom: 7,
+      // backgroundColor: '#EEEEEE',
+    },
+    name: {
+      fontWeight: 'bold',
+      fontSize: scaleFontSize(16),
+      color: '#374957',
+    },
+    relationshipAndDate: {
+      color: '#939393',
+      fontSize: scaleFontSize(16),
+    },
+    content: {
+      color: '#374957',
+      fontSize: scaleFontSize(16),
+      lineHeight: scaleFontSize(24),
+      paddingTop: 10,
+    },
+    editAndDeleteContainer: {
+      flexDirection: 'row',
+      justifyContent: 'flex-end',
+      alignItems: 'center',
+      width: '100%',
+    },
+    actionButtonsContainer: {flexDirection: 'row', justifyContent: 'flex-end'},
+    seeLessOrMore: {
+      title: {
+        color: '#939393',
+        fontSize: scaleFontSize(14),
+        lineHeight: scaleFontSize(24),
+      },
+    },
+  },
   plusButtonContainer: {
     marginLeft: Dimensions.get('window').width * 0.03,
     marginRight: Dimensions.get('window').width * 0.07,
