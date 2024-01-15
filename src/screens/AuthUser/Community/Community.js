cb2724c0999d269fb46f5a2b44b95a03998646dc
mpark1 <minspark@seas.upenn.edu>
2024-01-15 15:56:57 +0900

    updates

--- src/screens/AuthUser/Community/Community.js
+++ src/screens/AuthUser/Community/Community.js
@@ -1,7 +1,15 @@
-import React from 'react';
+import React, {useCallback} from 'react';
 import {SafeAreaView, Text, View} from 'react-native';
+import {useFocusEffect} from '@react-navigation/core';
 
-const Community = () => {
+const Community = ({navigation}) => {
+  useFocusEffect(
+    useCallback(() => {
+      console.log('community navigation is focused');
+      return () => console.log('community unfocused');
+    }, [navigation]), // Dependency array with navigation to make sure the effect runs when navigation changes
+  );
+
   return (
     <View>
       <Text>추모공간 탭의 첫 화면</Text>
