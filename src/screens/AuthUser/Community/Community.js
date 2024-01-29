46f2607839ec38bcfe5c9f86f22279942a84838d
mpark1 <minspark@seas.upenn.edu>
2024-01-29 18:40:03 +0900

    community search box

--- src/screens/AuthUser/Community/Community.js
+++ src/screens/AuthUser/Community/Community.js
@@ -1,20 +1,117 @@
-import React, {useCallback} from 'react';
-import {SafeAreaView, Text, View} from 'react-native';
+import React, {useEffect, useState} from 'react';
+import {
+  Dimensions,
+  FlatList,
+  ImageBackground,
+  SafeAreaView,
+  StyleSheet,
+  View,
+} from 'react-native';
+import globalStyle from '../../../assets/styles/globalStyle';
+import PetCard from '../../../components/PetCard';
+// import {queryPetsPagination} from '../../../utils/amplifyUtil';
+// import {useSelector} from 'react-redux';
+import SearchBox from '../../../components/SearchBox';
 import {useFocusEffect} from '@react-navigation/core';
+// import {petByName} from '../../../graphql/queries';
 
 const Community = ({navigation}) => {
+  const pageSize = 5;
+  // const userID = useSelector(state => state.user.cognitoUsername);
+  // const {myPets, readyForCommunityFetch} = useSelector(state => state.user);
+  const [petData, setPetData] = useState({
+    pets: [],
+    nextToken: null,
+  });
+  const [isLoadingPets, setIsLoadingPets] = useState(false);
+  const [isFetchComplete, setIsFetchComplete] = useState(false);
+  // console.log(
+  //   'print my pets in community page: ',
+  //   myPets.length,
+  //   readyForCommunityFetch,
+  // );
+  const [isSearchActive, setIsSearchActive] = useState(false);
+
+  const [resetSearchBox, setResetSearchBox] = useState(false);
+
   useFocusEffect(
-    useCallback(() => {
-      console.log('community navigation is focused');
-      return () => console.log('community unfocused');
-    }, [navigation]), // Dependency array with navigation to make sure the effect runs when navigation changes
+    React.useCallback(() => {
+      // This is executed when the screen gains focus
+      setResetSearchBox(false);
+
+      return () => {
+        setResetSearchBox(true);
+      };
+    }, []),
   );
 
+  const onEndReached = async () => {
+    // if (petData.nextToken !== null) {
+    //   await fetchPets();
+    // }
+  };
+
+  // const fetchSearchedPets = async petName => {
+  //   const inputVariables = {
+  //     name: petName,
+  //     filter: {accessLevel: {eq: 'Public'}},
+  //     limit: pageSize,
+  //     nextToken: petData.token,
+  //   };
+  //   queryPetsPagination(
+  //     userID,
+  //     petByName,
+  //     inputVariables,
+  //     isLoadingPets,
+  //     setIsLoadingPets,
+  //     myPets,
+  //   ).then(data => {
+  //     const {pets, nextToken: newNextToken} = data;
+  //     setPetData(prev => ({
+  //       pets: [...prev.pets, ...pets],
+  //       nextToken: newNextToken,
+  //     }));
+  //   });
+  // };
+
   return (
-    <View>
-      <Text>추모공간 탭의 첫 화면</Text>
-    </View>
+    <SafeAreaView style={globalStyle.flex}>
+      <ImageBackground
+        style={styles.backgroundImage}
+        source={require('../../../assets/images/milkyWayBackgroundImage.png')}
+        resizeMode={'cover'}>
+        <SearchBox
+          fetchSearchedPets={null}
+          setIsSearchActive={setIsSearchActive}
+          resetSearch={resetSearchBox}
+        />
+        {isFetchComplete && petData.pets.length > 0 && (
+          <View style={styles.flatListContainer}>
+            <FlatList
+              onMomentumScrollBegin={() => setIsLoadingPets(false)}
+              onEndReachedThreshold={0.8}
+              onEndReached={onEndReached}
+              showsVerticalScrollIndicator={false}
+              data={petData.pets}
+              renderItem={({item}) => <PetCard item={item} isFamily={false} />}
+            />
+          </View>
+        )}
+      </ImageBackground>
+    </SafeAreaView>
   );
 };
 
 export default Community;
+
+const styles = StyleSheet.create({
+  backgroundImage: {
+    flex: 1,
+  },
+  flatListContainer: {
+    paddingTop: Dimensions.get('window').height * 0.02,
+    flex: 1,
+    backgroundColor: 'transparent',
+    alignItems: 'center',
+  },
+});
